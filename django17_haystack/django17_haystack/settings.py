0f821b014011fc1934045e08be3de2c8e2ccc026
Anna Balcerzak <Nefernefernefer@Annas-MacBook-Pro.local>
2014-10-12 16:36:42 -0400

    install haystack

--- django17_haystack/django17_haystack/settings.py
+++ django17_haystack/django17_haystack/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'haystack',
+
+    'mysearch',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +85,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+    },
+}
